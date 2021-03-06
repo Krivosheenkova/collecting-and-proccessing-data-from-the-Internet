5db625a881031719e8482dde8c10dbd06d038593
Alina Krivosheenkova <alina88lis@gmail.com>
2021-03-06 20:38:17 +0300

    function by country added

--- 1_parsing_api/lastfm_gettop.py
+++ 1_parsing_api/lastfm_gettop.py
@@ -10,21 +10,21 @@ api_key = '50551cce8939e186906838d29a6ece0f'
 u = UserAgent().random
 # token = "7z1Tz_0E-vHe2s5vbVp0vpDCr6cKPvCl"
 # http://www.last.fm/api/auth/?api_key=50551cce8939e186906838d29a6ece0f&token=7z1Tz_0E-vHe2s5vbVp0vpDCr6cKPvCl
-
 headers = {'UserAgent': u}
-url = f'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=sweden&api_key={api_key}&format=json'
-r = requests.get(url, headers=headers)
 
-def jprint(obj):
-    """
-    :param obj: .get response
-    :return: formatted string of JSON type
-    """
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
 
-# jprint(r.json())
+def get_top_artist_by_country(country):
+
+    url = f'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country={country.lower()}' \
+          f'&api_key={api_key}&format=json'
+    r = requests.get(url, headers=headers)
+    return r
+
+
+r = get_top_artist_by_country('Sweden')
+
 with open('lastfm_response.json', 'w') as f:
     json.dump(r.json(), f, sort_keys=True, indent=4)
+
 
 print('Done')
